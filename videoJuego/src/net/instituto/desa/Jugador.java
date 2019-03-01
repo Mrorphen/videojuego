c7cf51c5f664487778394da6f3e2e9dbc037bd26
casa <casa@casa-PC>
2019-03-01 12:40:01 +0100

    Creo metodo para cada movimiento

--- videoJuego/src/net/instituto/desa/Jugador.java
+++ videoJuego/src/net/instituto/desa/Jugador.java
@@ -7,15 +7,39 @@ public class Jugador {
 		return y;
 	}
 
+	private Jugador(int y, int x) {
+		super();
+		this.y = y;
+		this.x = x;
+	}
+
 	public void setY(int y) {
 		this.y = y;
+	}
+
+	private Jugador(int y, int x) {
+		super();
+		this.y = y;
+		this.x = x;
 	}
 
 	public int getX() {
 		return x;
 	}
 
+	private Jugador(int y, int x) {
+		super();
+		this.y = y;
+		this.x = x;
+	}
+
 	public void setX(int x) {
+		this.x = x;
+	}
+
+	private Jugador(int y, int x) {
+		super();
+		this.y = y;
 		this.x = x;
 	}
 
